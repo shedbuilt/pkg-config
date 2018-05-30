e3b12192a4181294bc9323b7aeb9087d1748a764
Auston Stewart <auston@shedbuilt.net>
2018-05-30 08:19:16 -1000

    Updating build script

--- build.sh
+++ build.sh
@@ -5,6 +5,11 @@ SHED_PKG_LOCAL_DOCDIR="/usr/share/doc/${SHED_PKG_NAME}-${SHED_PKG_VERSION}"
 ./configure --prefix=/usr              \
             --with-internal-glib       \
             --disable-host-tool        \
-            --docdir=/usr/share/doc/pkg-config-0.29.2
-make -j $SHED_NUM_JOBS
-make DESTDIR=$SHED_FAKE_ROOT install
+            --docdir=${SHED_PKG_LOCAL_DOCDIR} &&
+# Build and Install
+make -j $SHED_NUM_JOBS &&
+make DESTDIR="$SHED_FAKE_ROOT" install || exit 1
+# Optionally Remove Documentation
+if [ -z "${SHED_PKG_LOCAL_OPTIONS[docs]}" ]; then
+    rm -rf "${SHED_FAKE_ROOT}/usr/share/doc"
+fi
